33596fb3284812a14b57822b049f7874326c45ca
shotagithub <shota19891031@icloud.com>
2023-02-16 19:11:54 +0900

    image to images

--- app/controllers/illusts_controller.rb
+++ app/controllers/illusts_controller.rb
@@ -1,4 +1,6 @@
 class IllustsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :set_find, only: [:show, :destroy]
 
   def index
     @illust = Illust.includes(:user).limit(10).order("created_at DESC")
@@ -18,11 +20,20 @@ class IllustsController < ApplicationController
   end
 
   def show
-    @illust = Illust.find(params[:id])
+  end
+
+  def destroy
+    @illust.destroy if user_signed_in? && current_user.id == @illust.user.id
+    redirect_to root_path
   end
 
   private
   def illust_params
     params.require(:illust).permit(:title, :description, :application, :prompt, {images: []}).merge(user_id: current_user.id)
   end
+
+  def set_find
+    @illust = Illust.find(params[:id])
+  end
+
 end
