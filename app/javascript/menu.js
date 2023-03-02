2414aa50340461f7f983c3b1eb9c7d65de48a76f
shotagithub <shota19891031@icloud.com>
2023-03-02 15:54:32 +0900

    product_calc.js fix

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+const menu = () => {
 
   const navPullDown = document.getElementById("navBtn")
   const navPullDownParents = document.getElementById("navPullDown")
@@ -12,13 +12,12 @@ window.addEventListener('load', function(){
     })
   
     navPullDown.addEventListener('click', function() {
-      // プルダウンメニューの表示と非表示の設定
       if (navPullDownParents.getAttribute("style") == "display:block;") {
-        // pullDownParentsのstyle属性にdisplay:block;が指定されている場合（つまり表示されている時）実行される
         navPullDownParents.removeAttribute("style")
       } else {
-        // pullDownParentsのstyle属性にdisplay:block;が指定されていない場合（つまり非表示の時）実行される
         navPullDownParents.setAttribute("style", "display:block;")
       }
     })
-  })
+}
+
+window.addEventListener('load', menu)
--- app/javascript/product_calc.js
+++ app/javascript/product_calc.js
@@ -1,4 +1,4 @@
-function productCalc(){
+const productCalc = () => {
   const price = document.getElementById("product_price")
   const fee = document.getElementById("tax_price")
   const sum = document.getElementById("profit")
@@ -8,5 +8,6 @@ function productCalc(){
     sum.innerHTML = Math.floor(price.value * 0.9)
   });
 };
-
+if(document.URL.match(/products\/new/)){
 window.addEventListener('load', productCalc);
+};
