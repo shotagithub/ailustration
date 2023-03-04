5e4e1d6667a241ae9b2ea23a0cbe125e23cf4d88
shotagithub <shota19891031@icloud.com>
2023-03-04 12:58:07 +0900

    show all pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,19 +11,19 @@ class UsersController < ApplicationController
   def show_all_illusts
     user = User.find(params[:id])
     @user = user.nickname
-    @illust = user.illusts.limit(20).order(created_at: 'desc')
+    @illust = user.illusts.page(params[:page]).order(created_at: 'desc')
   end
 
   def show_all_novels
     user = User.find(params[:id])
     @user = user.nickname
-    @novel = user.novels.limit(20).order(created_at: 'desc')
+    @novel = user.novels.page(params[:page]).order(created_at: 'desc')
   end
 
   def show_all_products
     user = User.find(params[:id])
     @user = user.nickname
-    @product = user.products.limit(20).order(created_at: 'desc')
+    @product = user.products.page(params[:page]).order(created_at: 'desc')
   end
  
 end
