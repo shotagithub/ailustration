69dea7fd5bd490b7ca8e76e4b6fa04664b80a84b
shotagithub <shota19891031@icloud.com>
2023-03-07 08:46:23 +0900

    novel model test

--- spec/factories/novels.rb
+++ spec/factories/novels.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :novel do
-    
+    title         {"title"}
+    application   {"application"}
+    content       {"test"}
+    association :user
+
+    after(:build) do |novel|
+      novel.images.attach(io: File.open('app/assets/images/ailus_logo.png'), filename: 'ailus_logo.png')
+    end
   end
 end
--- spec/models/novel_spec.rb
+++ spec/models/novel_spec.rb
@@ -2,5 +2,44 @@ require 'rails_helper'
 # bundle exec rspec spec/models/novel_spec.rb
 # 要custom validate 対応
 RSpec.describe Novel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @novel = FactoryBot.build(:novel)
+  end
+
+  describe 'イラストの新規投稿' do
+    
+    context '新規投稿できる場合' do
+      it 'title,contentが存在すれば登録できる' do
+        expect(@novel).to be_valid
+      end
+
+
+      it 'applicationが空でも登録できる' do
+        @novel.application = ""
+        expect(@novel).to be_valid
+      end
+    end
+  
+
+    context '新規投稿できない場合' do
+
+      it 'ユーザーが紐づいていなければ投稿できない' do
+        @novel.user = nil
+        @novel.valid?
+        expect(@novel.errors.full_messages).to include('User must exist')
+      end
+
+      it 'titleが空では登録できない' do
+        @novel.title = ""
+        @novel.valid?
+        expect(@novel.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'contentが空では登録できない' do
+        @novel.content = ""
+        @novel.valid?
+        expect(@novel.errors.full_messages).to include("Content can't be blank")
+      end
+    end
+  end
 end
